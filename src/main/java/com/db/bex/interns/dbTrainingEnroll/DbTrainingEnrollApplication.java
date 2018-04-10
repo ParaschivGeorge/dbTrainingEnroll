22d5649a8fb3f3cb2904d595ab6e560583e6225c
Paraschiv George <georgegmtt@gmail.com>
2018-04-10 16:40:19 +0300

    start

---
+++ src/main/java/com/db/bex/interns/dbTrainingEnroll/DbTrainingEnrollApplication.java
@@ -0,0 +1,12 @@
+package com.db.bex.interns.dbTrainingEnroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbTrainingEnrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbTrainingEnrollApplication.class, args);
+	}
+}
