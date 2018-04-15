956e12e36abcc76d17df9e73f0bf99419562c1de
Maria Lungu <anauna.lungu@gmail.com>
2018-04-15 18:30:32 +0300

    DBTRAIN-17 red - add controller mapping to app context

--- src/main/java/com/db/bex/interns/dbTrainingEnroll/DbTrainingEnrollApplication.java
+++ src/main/java/com/db/bex/interns/dbTrainingEnroll/DbTrainingEnrollApplication.java
@@ -2,8 +2,10 @@ package com.db.bex.interns.dbTrainingEnroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.db.bex.interns"} )
 public class DbTrainingEnrollApplication {
 
 	public static void main(String[] args) {
