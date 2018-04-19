42ee4adb5fb0511403ef225ddc30109bf462c80e
Paraschiv George <georgegmtt@gmail.com>
2018-04-19 21:16:39 +0300

    security repair

--- src/main/java/com/db/bex/dbTrainingEnroll/DbTrainingEnrollApplication.java
+++ src/main/java/com/db/bex/dbTrainingEnroll/DbTrainingEnrollApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.db.bex.dbTrainingEnroll"} )
+@ComponentScan(basePackages = { "com.db.bex.dbTrainingEnroll"},
+		excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class)
+})
 @EnableJpaRepositories("com.db.bex.dbTrainingEnroll")
 @EntityScan("com.db.bex.dbTrainingEnroll.entity")
 public class DbTrainingEnrollApplication {
