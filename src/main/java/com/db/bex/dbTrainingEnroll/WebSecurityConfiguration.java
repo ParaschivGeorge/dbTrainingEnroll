4ebc16a866f6de5459f6469b5d71413501b8c267
Paraschiv George <georgegmtt@gmail.com>
2018-04-20 13:14:54 +0300

    security settings allow operations

---
+++ src/main/java/com/db/bex/dbTrainingEnroll/WebSecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.db.bex.dbTrainingEnroll;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/index.html").permitAll()
+//                .antMatchers("/persons/**").hasRole("ADMIN")
+//                .and()
+//                .httpBasic()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/persons")
+//                .and()
+//                .exceptionHandling().accessDeniedPage("/login")
+////            .exceptionHandling().accessDeniedHandler(accessDeniedHandler())
+//                .and()
+//                .logout().permitAll();
+
+        http
+                .httpBasic().and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/**").authenticated()
+                .antMatchers(HttpMethod.PUT, "/**").authenticated()
+                .antMatchers(HttpMethod.PATCH, "/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/**").authenticated();
+    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin").password("{noop}admin").roles("ADMIN")
+//                .and()
+//                .withUser("user").password("{noop}password").roles("USER");
+//    }
+}
