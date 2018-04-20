dc6ca2b675b0f27abcd2142b1276cd4f310bfb11
Paraschiv George <georgegmtt@gmail.com>
2018-04-20 11:34:15 +0300

    dummy Post

--- src/main/java/com/db/bex/dbTrainingEnroll/controller/DummyController.java
+++ src/main/java/com/db/bex/dbTrainingEnroll/controller/DummyController.java
@@ -3,9 +3,7 @@ package com.db.bex.dbTrainingEnroll.controller;
 import com.db.bex.dbTrainingEnroll.dto.DummyDto;
 import com.db.bex.dbTrainingEnroll.service.DummyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
@@ -23,5 +21,10 @@ public class DummyController{
     public @ResponseBody
     List<DummyDto> search(Principal principal){
         return dummyService.search();
+    }
+
+    @PostMapping("/dummypost")
+    public String test(@RequestBody String data) {
+        return data + "aaa";
     }
 }
