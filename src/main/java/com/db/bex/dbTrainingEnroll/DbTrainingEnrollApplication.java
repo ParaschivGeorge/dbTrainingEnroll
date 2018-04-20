7fa03ddff4bc46af62c508ee57a060a86d34802f
Paraschiv George <georgegmtt@gmail.com>
2018-04-20 14:29:57 +0300

    security settings allow operations

--- src/main/java/com/db/bex/dbTrainingEnroll/DbTrainingEnrollApplication.java
+++ src/main/java/com/db/bex/dbTrainingEnroll/DbTrainingEnrollApplication.java
@@ -4,15 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.db.bex.dbTrainingEnroll"},
-		excludeFilters = {
-		@ComponentScan.Filter(type = FilterType.ANNOTATION, value = Configuration.class)
-})
+@ComponentScan(basePackages = { "com.db.bex.dbTrainingEnroll"})
 @EnableJpaRepositories("com.db.bex.dbTrainingEnroll")
 @EntityScan("com.db.bex.dbTrainingEnroll.entity")
 public class DbTrainingEnrollApplication {
