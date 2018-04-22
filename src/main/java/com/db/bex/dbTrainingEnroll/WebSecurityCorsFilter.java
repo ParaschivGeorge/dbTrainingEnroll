b7ddea1046f8843f19a48c2264cfaa61d02f1d9a
Paraschiv George <georgegmtt@gmail.com>
2018-04-22 17:02:14 +0300

    auth filter back

--- src/main/java/com/db/bex/dbTrainingEnroll/WebSecurityCorsFilter.java
+++ src/main/java/com/db/bex/dbTrainingEnroll/WebSecurityCorsFilter.java
@@ -5,6 +5,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -14,15 +15,35 @@ public class WebSecurityCorsFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        HttpServletResponse res = (HttpServletResponse) response;
+//        res.setHeader("Access-Control-Allow-Origin", "*");
+//        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+//        res.setHeader("Access-Control-Max-Age", "3600");
+//        res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, x-requested-with, Cache-Control");
+//        if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) request).getMethod())) {
+//            res.setStatus(HttpServletResponse.SC_OK);
+//        } else {
+//            chain.doFilter(request, response);
+//        }
+//        chain.doFilter(request, res);
+//    }
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-        res.setHeader("Access-Control-Max-Age", "3600");
-        res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, x-requested-with, Cache-Control");
-        chain.doFilter(request, res);
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        final HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
     }
+
     @Override
     public void destroy() {
     }
