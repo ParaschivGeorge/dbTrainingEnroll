cd66fab7b6b6e119b2b18c772e99246413e15fc2
Paraschiv George <georgegmtt@gmail.com>
2018-04-22 13:58:07 +0300

    mail should be unique

--- src/main/java/com/db/bex/dbTrainingEnroll/entity/User.java
+++ src/main/java/com/db/bex/dbTrainingEnroll/entity/User.java
@@ -18,7 +18,7 @@ public class User {
     @Column(nullable = false, length = 100)
     private String name;
 
-    @Column(nullable = false, length = 50)
+    @Column(unique = true, nullable = false, length = 50)
     private String mail;
 
     @Column(nullable = false)
