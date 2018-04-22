dab3f18581360df5251c2033a4a25c804bf61eb5
Paraschiv George <georgegmtt@gmail.com>
2018-04-22 16:12:14 +0300

    cors fix 4

--- src/main/java/com/db/bex/dbTrainingEnroll/WebSecurityCorsFilter.java
+++ src/main/java/com/db/bex/dbTrainingEnroll/WebSecurityCorsFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 public class WebSecurityCorsFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
--- src/main/java/com/db/bex/dbTrainingEnroll/security/JwtAuthorizationTokenFilter.java
+++ src/main/java/com/db/bex/dbTrainingEnroll/security/JwtAuthorizationTokenFilter.java
@@ -9,13 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 import io.jsonwebtoken.ExpiredJwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class JwtAuthorizationTokenFilter extends OncePerRequestFilter {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
