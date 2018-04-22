020a255e6b15524679b53cac04bdc9d22c4d87dc
Maria Lungu <anauna.lungu@gmail.com>
2018-04-22 15:03:49 +0300

    Fixing cross origins on front-end - try 1

--- src/main/java/com/db/bex/dbTrainingEnroll/controller/TrainingController.java
+++ src/main/java/com/db/bex/dbTrainingEnroll/controller/TrainingController.java
@@ -21,6 +21,7 @@ public class TrainingController {
         return trainingService.findTrainings();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/pendingTrainings")
     public List<TrainingDto> getPendingTrainings(@RequestBody EmailDto email){
         return trainingService.findPendingTrainings(email);
