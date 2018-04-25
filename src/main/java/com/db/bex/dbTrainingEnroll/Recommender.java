79b5dbdcd65352894a15055d2ccafcb310ca0d18
Paraschiv George <georgegmtt@gmail.com>
2018-04-25 12:44:59 +0300

    refactor

--- src/main/java/com/db/bex/dbTrainingEnroll/Recommender.java
+++ src/main/java/com/db/bex/dbTrainingEnroll/Recommender.java
@@ -5,13 +5,10 @@ import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.recommender.CachingRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.GenericItemSimilarity;
 import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -21,16 +18,15 @@ import java.util.List;
 @Component
 public class Recommender {
 
-    public Recommender(TrainingRepository trainingRepository)
-    {
+    public Recommender(TrainingRepository trainingRepository) {
         try {
             DataModel model = new FileDataModel(new File("E:\\date1.csv"));
 //            ItemSimilarity similarity = new PearsonCorrelationSimilarity(model);
             ItemSimilarity similarity = new LogLikelihoodSimilarity(model);
             GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(model, similarity);
             CachingRecommender recommender1 = new CachingRecommender(recommender);
-            List<RecommendedItem> recommendedItemList = recommender1.recommend(4,2);
-            for(RecommendedItem i : recommendedItemList)
+            List<RecommendedItem> recommendedItemList = recommender1.recommend(4, 2);
+            for (RecommendedItem i : recommendedItemList)
                 System.out.println(trainingRepository.findById(i.getItemID()));
         } catch (IOException e) {
             e.printStackTrace();
